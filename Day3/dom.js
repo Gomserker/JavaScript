3f32bac5c8418ce4abd6e5dab09526bce1a00453
byeong gwan <gomserker@gmail.com>
2021-10-27 10:58:17 +0900

    studied

---
+++ Day3/dom.js
@@ -0,0 +1,79 @@
+// HTML의 엘리먼트들을 JS로 변경하고 읽을 수 있다.
+//  Object란?
+
+//document
+
+//document 또한 html을 가리키는 객체(object)
+
+//document.title을 브라우저 콘솔에 입력하면 index.html의 타이틀이 나온다.
+
+// document.title = "hello";
+// // js에서 변경한 타이틀 이름으로 변경된다.
+// // js에서 html을 변경할 수 있다는 것이다.
+
+
+// document.getElementById("title")
+// //getElementById -> HTML의 엘리먼트를 ID 값으로 호출하는 것. 하지만 직접 보여주지는 않음.
+
+// const title = document.getElementById("title");
+
+// console.dir(title);
+
+// console.log(title.id);
+// console.log(title.className);
+// title.innerText = "Got you!"
+
+
+// const hellos = document.getElementsByClassName("hello");
+
+// console.log(hellos);
+
+// console.log(hellos[0]);
+
+// const title2 = document.getElementsByTagName(h1);
+
+//querySelector -> CSS 문법으로 엘리먼트를 찾을 수 있다.
+//단 중복된 클래스명이 있을 경우엔 가장 첫번째만 호출된다.
+//전부 가져오려면 querySelecorAll을 사용한다.
+
+const title = document.querySelector(".hello h1");
+
+
+console.log(title);
+title.innerText = "hello";
+
+title.style.color = "blue";
+
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+    title.style.color = "tomato";
+    return false;
+}
+
+function handleMouseEnter(){
+    title.style.color = "teal";
+    title.innerText = "mouse is on me!"
+}
+
+function handleMouseLeave(){
+    title.style.color = "black";
+    title.innerText = "mouse has left"
+}
+title.addEventListener("click", handleTitleClick);
+// title.onclick = handleTitleClick; 완전히 같은 기능
+
+title.addEventListener("mouseenter", handleMouseEnter);
+// title.onmouseenter = handleMouseEnter;
+title.addEventListener("mouseleave", handleMouseLeave);
+
+
+console.dir(title);
+//addEventListener -> function을 바로 실행시키지 않고, 특정 event가 발생했을때 해당 function을 실행하도록 한다.
+
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+window.addEventListener("resize", handleWindowResize);
