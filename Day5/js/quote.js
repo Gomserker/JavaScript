c2ca63b06e8a36436251676c365ea0d65ae2c422
byeong gwan <gomserker@gmail.com>
2021-11-28 23:21:26 +0900

    css added

--- Day5/js/quote.js
+++ Day5/js/quote.js
@@ -1,44 +1,13 @@
 const quotes = [
     {
-        quote: "quote1",
-        author: "a;osidufpoisauerasd",
+        quote: "If your pictures aren't good enough, you aren't close enough.",
+        author: "Robert Capa",
     },
     {
-        quote: "quote2",
-        author: "a;osid4987ufpoisauerasd",
+        quote: "Photography takes an instant out of time, altering life by holding it still.",
+        author: "Dorothea Lange",
     },
-    {
-        quote: "quote3",
-        author: "a;osidufpoisauerasd",
-    },
-    {
-        quote: "quote4",
-        author: "a;osidufpoisauerasd",
-    },
-    {
-        quote: "quote5",
-        author: "a;osid98798654654ufpoisauerasd",
-    },
-    {
-        quote: "quote6",
-        author: "a;osid98798654654ufpoisauerasd",
-    },
-    {
-        quote: "quote7",
-        author: "a;osid98798654654ufpoisauerasd",
-    },
-    {
-        quote: "quote8",
-        author: "a;osid98798654654ufpoisauerasd",
-    },
-    {
-        quote: "quote9",
-        author: "a;osid98798654654ufpoisauerasd",
-    },
-    {
-        quote: "quote10",
-        author: "a;osid98798654654ufpoisauerasd",
-    },
+    
 ]
 
 const quote = document.querySelector("#quote span:first-child");
