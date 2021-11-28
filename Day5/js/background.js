595e214865aec2b60716ccba19025ea2f3bf1965
byeong gwan <gomserker@gmail.com>
2021-11-28 22:24:19 +0900

    adding css

--- Day5/js/background.js
+++ Day5/js/background.js
@@ -5,5 +5,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add("image");
 
 document.body.appendChild(bgImage);
